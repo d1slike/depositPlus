61283652d8d3c156e0f6ec4bb35890ecd1551b08
d1slike <eldustru@yandex.ru>
2015-04-27 08:35:19 +0400

    *мелочи

--- utils/array.h
+++ utils/array.h
@@ -6,7 +6,7 @@ template <class T=int, int SIZE = 100> class Array
 {
     T array[SIZE];
 public:
-    T& operator[](register int i)
+    T& operator[](int i)
     {
         if(i >= 0 && i < SIZE)
             return *(array+i);
