fa7d01e77e4038d240ecc63e63186a209080b62b
Shprechen <olandreyts@rambler.ru>
2015-05-28 02:19:07 +0300

    *подправил конструтор

--- calc_logic/model/deposit.h
+++ calc_logic/model/deposit.h
@@ -12,11 +12,12 @@ class Deposit
     Date date;
     int day_count;
 public:
-    Deposit(const Money& sum, int day_count, DepositTemplate templ)
+    Deposit(Money sum, int day_count, DepositTemplate templ,Date date)
     {
         this->day_count = day_count;
         this->sum = sum;
         this->templ = templ;
+        this->date=date;
     }
     Money Calc();
     Money CalcCap(double,Money ,int);
