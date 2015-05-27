58186e399772a4354b890cea7d82d460bd33fb24
Shprechen <olandreyts@rambler.ru>
2015-05-27 21:28:26 +0300

    Сделаны 2 функции

--- calc_logic/model/deposit.h
+++ calc_logic/model/deposit.h
@@ -2,13 +2,14 @@
 #include "valute.h"
 #include "date.h"
 #include "money.h"
-
+#include "math.h"
 //конкртеный экземпляр вклада
 //служит для расчетов
 class Deposit
 {
     DepositTemplate templ;
     Money sum;
+    Date date;
     int day_count;
 public:
     Deposit(const Money& sum, int day_count, DepositTemplate templ)
@@ -17,5 +18,8 @@ public:
         this->sum = sum;
         this->templ = templ;
     }
+    Money Calc();
+    Money CalcCap(double,Money ,int);
+    Money CalcNoCap(double,Money ,int);
 };
 
