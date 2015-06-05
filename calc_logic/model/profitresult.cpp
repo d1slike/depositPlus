ff0d5e4bc2aa5087b6f58d08ade71111737ea873
Shprechen <olandreyts@rambler.ru>
2015-06-05 17:08:54 +0300

    + 2 переменные под рейты

--- calc_logic/model/profitresult.cpp
+++ calc_logic/model/profitresult.cpp
@@ -7,6 +7,18 @@ ProfitResult::ProfitResult(Date& remove_date, m_long sum, m_long profit)
     this->sum = sum;
 }
 
+ProfitResult::ProfitResult(Date &close_date, m_long sum, m_long profit, double rates_low, double rates_up)
+{
+    this->profit = profit;
+    this->close_date = close_date;
+    this->sum = sum;
+    this->rates_low=rates_low;
+    if(rates_low!=rates_up)
+        this->rates_up=rates_up;
+    else
+        this->rates_up=0;
+}
+
 QString ProfitResult::toString()
 {
     QString s = "Дата закрытия вклада: " + close_date.toString() + "\n";
--- calc_logic/model/profitresult.h
+++ calc_logic/model/profitresult.h
@@ -12,11 +12,14 @@ class ProfitResult
     Date close_date; //дата закрытия вклада, если установлен влаг досрочного закрытия, то устанавливается соответствующая дата
     m_long sum; //сумма в день закрытия вклада
     m_long profit; // прибыль
+    double rates_low;// нижний %
+    double rates_up=0;// верхний % по умолчанию 0
 
 
 public:
     ProfitResult(){}
     ProfitResult(Date& close_date, m_long sum, m_long profit);
+    ProfitResult(Date& close_date, m_long sum, m_long profit,double rates_low ,double rates_up);
     bool operator >(ProfitResult& tmp);
     QString toString(); //вывод на интерфейс
 };
